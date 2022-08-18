9fc1a91af7ad5d18186a60dbbed7b1915a7cc4df
Jordan Darlington <jordan@darlo.dev>
2022-08-18 22:11:52 +0100

    added variables

---
+++ 00_introduction/02_variables.ts
@@ -0,0 +1,9 @@
+// VARIABLES
+
+// variables are assigned types in TypeScript, this is done in either an explicit or implicit way
+
+let variableOne: string = "one";
+// explicit - the type is written in the declaration
+
+let variableTwo = "two";
+// implicit - the type is guessed from the assigned value
